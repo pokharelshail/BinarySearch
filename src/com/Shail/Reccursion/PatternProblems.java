10802d0fd94fe344ed64b6d2f279f7b6f27cdf7b
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2023-07-12 00:17:54 -0500

    Reccursion Prolems

---
+++ src/com/Shail/Reccursion/PatternProblems.java
@@ -0,0 +1,31 @@
+package com.Shail.Reccursion;
+
+public class PatternProblems {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    If n is 5
+
+     * * * * *
+     * * * *
+     * * *
+     * *
+     *
+
+     */
+    static void printRevTraingle(int n){}
+
+    static  void printTriangle(int n){}
+
+    /* Add more parameters as needed*/
+    static int[] bubbleSort(int[] arr){
+        return arr;
+    }
+
+    /* Add more parameters as needed*/
+    static int[] selectionSort(int[] arr){
+        return arr;
+    }
+}
---
+++ src/com/Shail/Reccursion/ReverseNum.java
@@ -0,0 +1,16 @@
+package com.Shail.Reccursion;
+
+import java.sql.SQLOutput;
+
+public class ReverseNum {
+
+    static int sum =0;
+    public static void main(String[] args) {
+        reverseNum(1234);
+        System.out.println(sum);
+    }
+
+    static void reverseNum(int n){
+
+    }
+}
