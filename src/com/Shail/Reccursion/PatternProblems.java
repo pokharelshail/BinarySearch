589bded08069aa718a9d640b74449de479f81419
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2023-07-12 19:27:52 -0500

    Reccursion Solutions. Bubblesort reccursive way

--- src/com/Shail/Reccursion/PatternProblems.java
+++ src/com/Shail/Reccursion/PatternProblems.java
@@ -1,8 +1,13 @@
 package com.Shail.Reccursion;
 
+import java.util.Arrays;
+
 public class PatternProblems {
     public static void main(String[] args) {
-
+        printRevTraingle(5,0);
+        printTriangle(5,0);
+        int[] arr = {4,3,2,1};
+        System.out.println(Arrays.toString(bubbleSort(arr,0, arr.length-1)));
     }
 
     /*
@@ -13,15 +18,49 @@ public class PatternProblems {
      * * *
      * *
      *
-
      */
-    static void printRevTraingle(int n){}
+    static void printRevTraingle(int n, int limit){
+        if (n==0){
+            System.out.print("* ");
+            return;
+        }
+        if (limit == n){
+            System.out.println();
+            printRevTraingle(n-1,0);
+        }else {
+            System.out.print("* ");
+            printRevTraingle(n, limit + 1);
+        }
+    }
 
-    static  void printTriangle(int n){}
+    static  void printTriangle(int n, int limit){
+        if (n==1){
+            return;
+        }
+        if (limit == n){
+            printTriangle(n-1,0);
+            System.out.println();
+        }else {
+            printTriangle(n, limit + 1);
+            System.out.print("* ");
+        }
+    }
 
     /* Add more parameters as needed*/
-    static int[] bubbleSort(int[] arr){
-        return arr;
+    static int[] bubbleSort(int[] arr, int index, int stop){
+        if (stop ==0){
+            return arr;
+        }
+        if (index == stop){
+           return bubbleSort(arr, 0, stop-1);
+        }else{
+            if (arr[index] > arr[index+1]){
+                int temp = arr[index];
+                arr[index] = arr[index+1];
+                arr[index+1] = temp;
+            }
+            return bubbleSort(arr, index+1 ,stop);
+        }
     }
 
     /* Add more parameters as needed*/
--- src/com/Shail/Reccursion/ReverseNum.java
+++ src/com/Shail/Reccursion/ReverseNum.java
@@ -8,9 +8,26 @@ public class ReverseNum {
     public static void main(String[] args) {
         reverseNum(1234);
         System.out.println(sum);
+        System.out.println(revNum(1234));
+
     }
 
     static void reverseNum(int n){
+        if (n ==0){
+            return;
+        }
+        int rem = n%10;
+        sum = sum*10 + rem;
+        reverseNum(n/10);
+    }
 
+    static int revNum(int n){
+        if (n ==0){
+            return 0;
+        }
+        int num_digits = (int) (Math.log10(n))+1;
+        int rem = (int) (n%10 * Math.pow(10,num_digits-1));
+        System.out.println(rem);
+        return rem + revNum(n/10);
     }
 }
