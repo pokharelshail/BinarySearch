c835055e80e71dc5c222c69d6e67cb02eaa9214e
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2023-07-18 00:02:45 -0500

    Subsequence arraylist return

--- src/com/Shail/Reccursion/Subset.java
+++ src/com/Shail/Reccursion/Subset.java
@@ -1,9 +1,26 @@
 package com.Shail.Reccursion;
 
+import java.util.ArrayList;
+
 public class Subset {
 
     public static void main(String[] args) {
         printSubsets("","abc");
+        ArrayList<String> subsetList = subsequence("","abc");
+        System.out.println(subsetList);
+    }
+
+    private static ArrayList<String> subsequence(String p, String up) {
+        if (up.length() ==0){
+            ArrayList<String> list= new ArrayList<String>();
+            list.add(p);
+            return list;
+        }
+        char firstChar = up.charAt(0);
+        ArrayList<String> left =subsequence(p+firstChar,up.substring(1));
+        ArrayList<String> right =subsequence(p,up.substring(1));
+        left.addAll(right);
+        return left;
     }
 
     private static void printSubsets(String p, String up) {
